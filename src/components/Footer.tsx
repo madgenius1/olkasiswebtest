fbf22bf1cdac02971597be29f75397d79f7d8cb8
madgenius1 <nickjuma@outlook.com>
2026-06-29 00:02:38 +0300

    Modified package.json         modified:   pnpm-lock.yaml         modified:   src/app/globals.css         modified:   src/app/layout.tsx         modified:   src/app/page.tsx added global.d.ts

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer () {
+    return(
+        <div className="p-4">
+            Navbar
+        </div>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar () {
+    return(
+        <div className="p-4">
+            Navbar
+        </div>
+    )
+}
